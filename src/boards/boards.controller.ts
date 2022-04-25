0bf922e779db2fdd8866ccbed53f9d24b71ce422
park_so_young <soyoung931014@gmail.com>
2022-04-25 20:54:20 +0900

    ADD: 데이터베이스를 이용한 CRUD구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,45 +1,81 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
-    // 서베스는 dependancy Injection을 해야 사용이 가능하다. 
     constructor(private boardsService: BoardsService) { }
 
     @Get()
-    getAllBoards(): Board[] {
+    getAllBoard(): Promise<Board[]> {
         return this.boardsService.getAllBoards();
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
     }
 
     @Post()
     @UsePipes(ValidationPipe)
-    createBoards(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
+    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
         return this.boardsService.createBoard(createBoardDto)
     }
 
-    //localhost:5000?id=dlfeks&title=fldie
-    @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardsService.getBoardById(id)
-    }
-
     @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void {
-        //리턴값이 없으므로 void 타입으로 정의해준다. 
-        this.boardsService.deleteBoard(id)
+    deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.boardsService.deleteBoard(id)
     }
 
     @Patch('/:id/status')
     updateBoardStatus(
-        @Param('id') id: string,
+        @Param('id', ParseIntPipe) id: number,
         @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    ): Board {
+    ): Promise<Board> {
         return this.boardsService.updateBoardStatus(id, status)
     }
-
 }
+
+
+
+// export class BoardsController {
+//     // 서베스는 dependancy Injection을 해야 사용이 가능하다. 
+//     constructor(private boardsService: BoardsService) { }
+
+//     @Get()
+//     getAllBoards(): Board[] {
+//         return this.boardsService.getAllBoards();
+//     }
+
+//     @Post()
+//     @UsePipes(ValidationPipe)
+//     createBoards(
+//         @Body() createBoardDto: CreateBoardDto
+//     ): Board {
+//         return this.boardsService.createBoard(createBoardDto)
+//     }
+
+//     //localhost:5000?id=dlfeks&title=fldie
+//     @Get('/:id')
+//     getBoardById(@Param('id') id: string): Board {
+//         return this.boardsService.getBoardById(id)
+//     }
+
+//     @Delete('/:id')
+//     deleteBoard(@Param('id') id: string): void {
+//         //리턴값이 없으므로 void 타입으로 정의해준다. 
+//         this.boardsService.deleteBoard(id)
+//     }
+
+//     @Patch('/:id/status')
+//     updateBoardStatus(
+//         @Param('id') id: string,
+//         @Body('status', BoardStatusValidationPipe) status: BoardStatus
+//     ): Board {
+//         return this.boardsService.updateBoardStatus(id, status)
+//     }
+
+// }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,48 +1,110 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid'
 import { CreateBoardDto } from './dto/create-board.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
+import { resolve } from 'path/posix';
 @Injectable()
 export class BoardsService {
-    private boards: Board[] = [];
-
-    getAllBoards(): Board[] {
-        return this.boards;
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository,
+    ) { }
+    // 모두 조회
+    async getAllBoards(): Promise<Board[]> {
+        return this.boardRepository.find();
     }
 
-    createBoard(createBoardDto: CreateBoardDto): Board {
-        const { title, description } = createBoardDto
-        const board: Board = {
-            id: uuid(),
-            title: title,
-            description: description,
-            status: BoardStatus.PUBLIC // 이거는 인젝션 안해도 선언 안해줘도 된다. 어차피 public, private만 사용하는거니까 
-        }
-        this.boards.push(board);
-        return board;
-    }
-
-    getBoardById(id: string): Board {
-        const found = this.boards.find((board) => board.id === id)
+    //조회
+    async getBoardById(id: number): Promise<Board> {
+        const found = await this.boardRepository.findOne(id)
 
         if (!found) {
-            throw new NotFoundException('너가 선택한 아이디 없어');
+            throw new NotFoundException(`${id}를 찾을 수 없어요`)
         }
-        return found;
+        return found
     }
 
-    deleteBoard(id: string): void {
-        //리턴값을 안줄것이기 때문에 void
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter((board) => board.id !== found.id)
+    //생성
+    createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        return this.boardRepository.createBoard(createBoardDto);
+
+        // 여기부분이 repository에 들어간다. 
+        /*    const { title, description } = createBoardDto;
+
+        const board = this.boardRepository.create({
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        })
+
+        await this.boardRepository.save(board);
+        return board;
+    } */
+
     }
 
-    updateBoardStatus(id: string, status: BoardStatus) {
-        const selectedBoard = this.getBoardById(id);
-        selectedBoard.status = status;
-        return selectedBoard;
+    //삭제
+    async deleteBoard(id: number): Promise<void> {
+        const result = await this.boardRepository.delete(id);
+        console.log('result', result)
+        if (result.affected === 0) {
+            throw new NotFoundException(`id:${id}를 찾을 수 없습니다.`)
+        }
+
     }
 
-
-
+    //수정하기
+    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+        const board = await this.getBoardById(id);
+        board.status = status;
+        await this.boardRepository.save(board);
+        return board;
+    }
 }
+
+// export class BoardsService {
+//     private boards: Board[] = [];
+
+//     getAllBoards(): Board[] {
+//         return this.boards;
+//     }
+
+//     createBoard(createBoardDto: CreateBoardDto): Board {
+//         const { title, description } = createBoardDto
+//         const board: Board = {
+//             id: uuid(),
+//             title: title,
+//             description: description,
+//             status: BoardStatus.PUBLIC // 이거는 인젝션 안해도 선언 안해줘도 된다. 어차피 public, private만 사용하는거니까 
+//         }
+//         this.boards.push(board);
+//         return board;
+//     }
+
+//     getBoardById(id: string): Board {
+//         const found = this.boards.find((board) => board.id === id)
+
+//         if (!found) {
+//             throw new NotFoundException('너가 선택한 아이디 없어');
+//         }
+//         return found;
+//     }
+
+//     deleteBoard(id: string): void {
+//         //리턴값을 안줄것이기 때문에 void
+//         const found = this.getBoardById(id);
+//         this.boards = this.boards.filter((board) => board.id !== found.id)
+//     }
+
+//     updateBoardStatus(id: string, status: BoardStatus) {
+//         const selectedBoard = this.getBoardById(id);
+//         selectedBoard.status = status;
+//         return selectedBoard;
+//     }
+
+
+
+// }
