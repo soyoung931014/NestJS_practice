606f8687fc955df503a9c03fd877d97c8a8f9b1b
park_so_young <soyoung931014@gmail.com>
2022-04-26 13:42:08 +0900

    ADD: 인증 기능 구현하기_비밀번호 암호화하기

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,15 +2,20 @@ import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
+
+        const salt = await bcrypt.genSalt(); // 솔트만들기
+        const hashedPassword = await bcrypt.hash(password, salt); // 솔트랑 패스워드랑 해시
+
         const user = this.create({
             username,
-            password
+            password: hashedPassword
         })
         try {
             await this.save(user)
